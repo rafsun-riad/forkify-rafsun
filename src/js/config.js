60a5bb5bdb8375cc32be70523be350514e3e87c3
rafsun-riad <mruhaquer@gmail.com>
2023-09-27 19:40:03 +0600

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "2bce6e4f-7613-46d8-a6d1-64104c76a020";
+export const MODAL_CLOSE_SEC = 2.5;
